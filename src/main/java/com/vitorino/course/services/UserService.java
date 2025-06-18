812e2fb57cae2c142f95ef422a702fac5ffeafaa
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-06-18 10:25:51 -0300

    Seervice layer, component registration

---
+++ src/main/java/com/vitorino/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.vitorino.course.services;
+
+import com.vitorino.course.Repositories.UserRepository;
+import com.vitorino.course.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
