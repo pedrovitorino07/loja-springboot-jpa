5d26fd8ad55a26c0a43e80c02314983275502247
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-06-20 15:56:44 -0300

    Exception handling - findById

--- src/main/java/com/vitorino/course/entities/Order.java
+++ src/main/java/com/vitorino/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.vitorino.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vitorino.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
@@ -17,6 +18,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     private Integer orderStatus;
